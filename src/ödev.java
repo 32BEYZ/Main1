5898b454da062b3e8cd6d437f74f3f4d56f34253
ABİDE BEYZA BAYRAM <abidebeyzabayram@icloud.com>
2022-09-18 13:01:42 +0300

    sesli harf
    kalın ince olma durumları

---
+++ src/ödev.java
@@ -0,0 +1,19 @@
+
+public class ödev {
+	char harf = "E";
+	switch(harf) {
+	   case"A";
+	    case "I";
+	    case"O";	
+	     case "U";
+	               System.out.println("kalın sesli harf");
+
+	               break;
+	               default:
+	            	   System.out.println("ince sesli harf");
+	
+	
+	
+	}
+
+}
